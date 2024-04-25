0e8b380322d8f93deff7a2cdcfd8160ac51d7169
Laurine Maina <mainalaureen58@gmail.com>
2024-04-25 17:21:54 +0300

    initial commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React from "react";
+function EyesOnMe(){
+    function Focus(){
+        console.log('Good!');
+    }
+    function Blur(){
+        console.log('Hey! Eyes on me!');
+    }
+    return(
+    <>
+    <button onFocus={Focus} onBlur={Blur}>Eyes on me</button>
+    </>
+    )
+}
+export default EyesOnMe
